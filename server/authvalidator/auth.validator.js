051b260107512c3043a0272f9a15c2a568e76c3a
Developer <dev@example.com>
2026-03-30 23:51:26 +0530

    authentication

---
+++ server/authvalidator/auth.validator.js
@@ -0,0 +1,26 @@
+const Joi = require('joi');
+const registerSchema = Joi.object({
+  email: Joi.string().email().required(),
+
+  password: Joi.string().min(6).uppercase(1).pattern(/[A-Z]/).pattern(/(?:.*\d){2,}/).required(),
+
+  confirmPassword: Joi.string()
+    .valid(Joi.ref('password'))
+    .required()
+    .messages({
+      'any.only': 'Passwords do not match'
+    })
+});
+const loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required()
+});
+const googleSchema = Joi.object({
+  idToken: Joi.string().required()
+});
+
+module.exports = {
+  registerSchema,
+  loginSchema,
+  googleSchema
+};
