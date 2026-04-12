1e29632c59b873ff285951d4f6677ba5365cbf69
Developer <dev@example.com>
2026-04-12 21:09:52 +0530

    frontend

--- client/code_ide/src/hooks/useStats.hooks.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getDashboardStats } from "../api/stats.api";
-
-export const useStatsQuery = () => {
-  return useQuery({
-    queryKey: ["stats"],
-    queryFn: getDashboardStats,
-  });
-};
--- client/code_ide/src/hooks/useUsers.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getUsers,type User } from "../api/auth.api";
-
-export const useUsers = () => {
-  return useQuery<User[]>({
-    queryKey: ["users"],
-    queryFn: getUsers
-  });
-};
